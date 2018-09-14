88f5346d22f63e9547c58d4c4b5c8ba25e0f95b1
Omar Nabolsi <on09@aub.edu.lb>
2018-09-14 17:11:42 +0300

    CRUD EF Posts

--- app.API/Controllers/PostsController.cs
+++ app.API/Controllers/PostsController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Threading.Tasks;
+using app.API.Data;
 using app.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace app.API.Controllers
 {
@@ -9,31 +11,77 @@ namespace app.API.Controllers
     [ApiController]
     public class PostsController : ControllerBase
     {
-        private readonly Post[] _posts;
-        public PostsController()
+        private readonly AppDbContext _context;
+        public PostsController(AppDbContext context)
         {
-            _posts = new Post[] {
-                new Post { Id = 1, Title = "Post 1", Content = " Content of post one", Created = DateTime.Now.AddDays(-3), LastUpdated = DateTime.Now.AddHours(-22) },
-                new Post { Id = 2, Title = "Post 2", Content = " Content of post two", Created = DateTime.Now.AddDays(-2), LastUpdated = DateTime.Now.AddHours(-18) },
-                new Post { Id = 3, Title = "Post 3", Content = " Content of post three", Created = DateTime.Now.AddDays(-1), LastUpdated = DateTime.Now.AddHours(-12) }
-            };
+            _context = context;
         }
 
         [HttpGet]
-        public IActionResult GetPosts()
+        public async Task<IActionResult> GetPosts()
         {
-            var posts = _posts;
+            var posts = await _context.Posts.ToListAsync();
             return Ok(posts);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetPost(int id)
+        public async Task<IActionResult> GetPost(int id)
         {
-            var post = Array.Find(_posts, p => p.Id == id);
+            var post = await _context.Posts.FirstOrDefaultAsync(p => p.Id == id);
             if (post == null)
                 return NotFound();
-                
+
             return Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(Post post)
+        {
+            post.Created = DateTime.Now;
+            post.LastUpdated = post.Created;
+
+            await _context.Posts.AddAsync(post);
+            var result = await _context.SaveChangesAsync();
+            if (result > 0)
+                return Ok(post);
+            return BadRequest("Failed to create post!");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePost(int id, Post post)
+        {
+            var postFromDb = await _context.Posts.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (postFromDb == null)
+                return NotFound();
+            
+            postFromDb.Title = post.Title;
+            postFromDb.Content = post.Content;
+            postFromDb.LastUpdated = DateTime.Now;
+
+            var result = await _context.SaveChangesAsync();
+
+            if (result > 0)
+                return Ok(postFromDb);
+            
+            return BadRequest($"Failed to update post {id}!");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            var postToDelete = await _context.Posts.FirstOrDefaultAsync(p => p.Id == id);
+            
+            if (postToDelete == null)
+                return NotFound();
+
+            _context.Posts.Remove(postToDelete);
+            var result = await _context.SaveChangesAsync();
+
+            if (result <= 0)
+                return BadRequest($"Failed to delete post {id}!");
+            
+            return NoContent();
         }
 
     }
